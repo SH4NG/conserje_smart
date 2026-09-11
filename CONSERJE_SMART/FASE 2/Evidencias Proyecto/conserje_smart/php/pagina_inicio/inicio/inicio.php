57b175ea33583bc1c5f2225b01c5f3a8259e9021
Mustankiller <134960147+Mustankiller@users.noreply.github.com>
2026-09-11 20:05:44 -0300

    pagina_inicio_1.0.3
    bugfix

---
+++ CONSERJE_SMART/FASE 2/Evidencias Proyecto/conserje_smart/php/pagina_inicio/inicio/inicio.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="/conserje_smart/css/pagina_inicio/inicio/inicio.css">
+</head>
+<body>
+    
+    <!-- TITULO PAGINA_INICIO  -->
+    <div class="inicio" id="inicio">
+
+
+        <!-- TITULO CONTENIDO_PAGINA_INICIO -->
+        <div class=titulo_inicio>
+            "TITULO DEL CONTENIDO DE INCIO"
+
+        </div>
+        
+        <!-- TITULO QUIENES SOMOS  -->
+
+            <!-- se llama al quienes_somos mediante include -->
+        <div class="contenedor_quienes_somos" id="contenedor_quienes_somos">
+            <?php include __DIR__ . '/quienes_somos/quienes_somos.php'; ?>
+    
+        </div>
+
+    </div>
+
+    <script><?php include __DIR__ . '/../../../js/pagina_inicio/inicio/inicio.js'; ?></script>
+
+</body>
+</html>
---
+++ CONSERJE_SMART/FASE 2/Evidencias Proyecto/conserje_smart/php/pagina_inicio/pie_pagina/logo_pie_pagina.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="/conserje_smart/css/pagina_inicio/pie_pagina/logo_pie_pagina.css">
+
+</head>
+<body>
+    
+    <!-- TITULO LOGO PIE PAGINA -->
+    <div class="logo_pie_pagina" id="logo_pie_pagina">
+
+        <!-- TITULO IMAGEN LOGO PIE PAGINA -->
+
+        <img src="/conserje_smart/imagenes/inicio_sesion/pie_pagina/logo_pie_pagina.png" alt="">
+        
+    </div>
+
+    <script src="/conserje_smart/js/pagina_inicio/pie_pagina/logo_pie_pagina.js"></script>
+</body>
+</html>
