b3a84444732ab9974eda0868c1ebebca26ae9bea
SH4NG <133914967+SH4NG@users.noreply.github.com>
2026-09-17 00:37:22 -0300

    creacion de estructura contenido inicio conserje

---
+++ CONSERJE_SMART/FASE 2/Evidencias Proyecto/conserje_smart/php/programa/conserje/inicio_conserje/contenedor_inicio_contenido/contenedor_inicio_contenido.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>inicio_conserje</title>
+    <link rel="stylesheet" href="/conserje_smart/css/programa/conserje/inicio_conserje/contenedor_inicio_contenido/contenedor_inicio_contenido.css">
+</head>
+    <body>
+        
+        <!-- TITULO 1 CONTENEDOR CONTENIDO INICIO -->
+
+            <!-- contenedor contenido inicio -->
+            <div  class="contenedor_contenido_inicio" id="contenedor_contenido_inicio">
+                
+                <!-- TITULO 2 PARTE SUPERIOR CONTENIDO INICIO -->
+
+                    <!-- parte superior contenido inicio -->
+                    <div class="parte_superior_contenido_inicio" id="parte_superior_contenido_inicio">
+                    
+                        <!-- TITULO 3 CONTENEDOR TITULO CONTENIDO -->
+
+                            <!-- contenedor que recibe titulo_contenido.php -->
+                            <div  class="contenedor_titulo_contenido" id="contenedor_titulo_contenido">
+
+                            </div>
+
+                        <!-- TITULO 4 CONTENEDOR FECHA CONTENIDO -->
+
+                            <!-- contenedor que recibe fecha_contenido.php -->
+                            <div class="contenedor_fecha_contenido" id="contenedor_fecha_contenido">
+
+                            </div>
+                            
+                        <!-- TITULO 5 CONTENEDOR SUBTITULO CONTENIDO -->
+
+                            <!-- contenedor que recibe subtitulo_contenido.php -->
+                            <div class="contenedor_subtitulo_contenido" id="contenedor_subtitulo_contenido">
+
+                            </div>
+
+                    </div>
+                
+                <!-- TITULO 6 PARTE INFERIOR CONTENIDO INICIO -->
+                 
+                    <!-- parte inferior contenido inicio -->
+                    <div class="parte_inferior_contenido_inicio" id="parte_inferior_contenido_inicio">
+                    
+                        <!-- TITULO 7 CONTENEDOR ACTIVIDADES CONTENIDO -->
+
+                            <!-- contenedor que recibe actividades_contenido.php -->
+                            <div class="contenedor_actividades_contenido" id="contenedor_actividades_contenido">
+
+                            </div>
+
+                        <!-- TITULO 8 CONTENEDOR ATAJOS CONTENIDO -->
+
+                            <!-- contenedor que recibe atajos_contenido.php -->
+                            <div class="contenedor_atajo_contenido" id="contenedor_atajo_contenido">
+
+                                <!-- TITULO 9 CONTENEDOR ACCESO RAPIDO -->
+
+                                    <!-- contenedor que recibe acceso_rapido.php -->
+                                    <div class="contenedor_acceso_rapido" id="contenedor_acceso_rapido">
+
+                                    </div>
+
+                                <!-- TITULO 10 CONTENEDOR CRONOGRAMA -->
+
+                                    <!-- contenedor que recibe cronograma.php -->
+                                    <div class="contenedor_cronograma" id="contenedor_cronograma">
+
+                                    </div>
+
+                            </div>
+
+                    </div>
+
+
+
+
+
+            </div>
+    
+
+
+
+
+
+    </body>
+</html>
